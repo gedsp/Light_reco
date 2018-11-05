7e60eb8db7dfa31add14b09b4f1560a82483b1aa
michael leyton <leyton@cern.ch>
2018-11-05 23:23:23 +0100

    macro for slaying file zombies

---
+++ dpdMaker/IsZombie.C
@@ -0,0 +1,16 @@
+#include "../config_reco.h"
+
+void IsZombie(int light_run)
+{	
+	TString infilename = Form("%s/light/dpd-light-%d.root",dpd_dir.c_str(),light_run);
+	
+	TFile ifile(infilename);
+	if (ifile.IsZombie()) cout << "Run " << light_run << " is a zombie" << endl;
+	else
+	{
+		TChain *t = new TChain("midas_data");
+		t->Add(infilename);
+		cout << "Run " << light_run << ": " << t->GetEntries() << " events" << endl;
+		ifile.Close();
+	}
+}
