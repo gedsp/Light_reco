9a6ab3833d1d50ddd3ff2f478d9ffd43fe44cd04
michael leyton <leyton@cern.ch>
2019-04-03 00:31:18 +0200

    new submission scripts for CONDOR

---
+++ dpdMaker/run_charge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+run=$1
+subrun=$2
+
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-slc6/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.04.18/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+
+echo "ls before macro: "
+ls --color -lh -c
+
+root -l -q -b "dpdMaker.C($run,$subrun)"
+
+#'("""+str(run)+""","""+str(subRun)+""")'      
+#root -l -b -q "macrogrid.C+g($i,$j,$k)"
+
+echo "ls after running macro: "
+ls --color -lh -c
+
+echo "done"
