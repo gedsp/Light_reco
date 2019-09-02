8d77d81af85dc432c76b8ca4022d13b6eea6ef7c
Gloria de Sa Pereira <gloria@desapereira.com>
2019-09-02 16:20:12 +0100

    modi files location

--- config_reco.h
+++ config_reco.h
@@ -17,24 +17,24 @@ static const float pmt_y_pos = 0.0;
 static const float pmt_z_pos[N_PMT]={50.0,100.0,150.0,200.0,250.0};
 
 // set up the input folder where the light-charge matched data is stored.
-const string matched_data_dir = "/Volumes/data/root_files/2018Aug24/"; // local 
+const string matched_data_dir = "/eos/user/l/leyton/dpd_20190419/matched/"; // local
 //const string matched_data_dir = "/eos/user/l/leyton/2018Aug24/"; // on lxplus
 
 // set up the input folder where the light-only data is stored.
-const string light_data_dir = "/Volumes/data/root_files/reprocessed/"; // local 
+const string light_data_dir = "/eos/user/l/leyton/dpd_20190419/light/"; // local
 //const string light_data_dir = "/eos/user/c/chlastor/ReprocessedData/21Feb/"; // on lxplus
 
 // set up the input folder where the Highway Algorithm data is stored 
-const string highway_data_dir = "/Volumes/data/root_files/Highway_VBW/"; // local
+const string highway_data_dir ="/afs/cern.ch/user/g/gldesape/WA105_mine/Analysis/Event-track-selection/HighwayAlgorithm"; // local
 //const string highway_data_dir = "/eos/experiment/wa105/offline/LArSoft/Data/Reco/2018_August_24/Highway_VBW/"; // on lxplus
 
 // full path to Light_reco directory                        
-const string reco_dir = "/Volumes/data/Dropbox/physics/311/git/Light_reco"; // local
+const string reco_dir = "/afs/cern.ch/user/g/gldesape/WA105_mine/Light_reco"; // local
 //const string reco_dir = "/afs/cern.ch/user/l/leyton/git/Light_reco"; // on lxplus 
 
 // full path to DPD storage
 //const string dpd_dir = "/afs/cern.ch/user/l/leyton/work/dpd_20190403"; // on lxplus
-const string dpd_dir = "/Volumes/data/Dropbox/physics/311/git/Light_reco/dpdMaker/dpd"; // local
+const string dpd_dir = "/afs/cern.ch/user/g/gldesape/WA105_mine/Light_reco/dpdMaker/dpd"; // local
 
 // full path to db files
 const string db_charge_file = reco_dir+"/dbVoltages_charge.root";
